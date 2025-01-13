8152ec129d354a1a309619628be3c8112ab34f44
lmmoreira2 <leonardo.moreira@impa.br>
2025-01-13 20:06:39 -0300

    Update Organizer

--- folder_organizer/Organizer.py
+++ folder_organizer/Organizer.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 here = os.getcwd()
 print(here)
@@ -6,3 +7,36 @@ print(here)
 list = os.listdir('.')
 
 print(list)
+
+# Check if log file exists
+
+if not os.path.exists('log/log.txt'):
+
+    # Create log file
+    os.makedirs('log')
+
+    log = open('log/log.txt', 'w')
+    log.write('Log file created at ' + time.ctime() + '\n\n\n' )
+    log.close()
+
+
+
+log = open('log/log.txt', 'a')
+for file in list:
+
+    if file == 'Organizer.py':
+        continue
+    if '.' not in file:
+        continue
+
+    tipo = file.split('.')[-1]
+    if not os.path.exists(tipo):
+        os.makedirs(tipo)
+    
+    os.rename(file, tipo + '/' + file)
+
+    log.write(file + ' moved to ' + tipo + ' at ' + time.ctime() + '\n')
+
+log.write('\n\n')
+
+log.close()
